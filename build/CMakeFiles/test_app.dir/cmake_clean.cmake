ecf13aedc9c1f0772c828f49f4b53bfc1d023704
Benjamin Navarro <navarro@lirmm.fr>
2015-10-12 17:18:01 +0200

    Modified build gitignore and removed build files

--- build/CMakeFiles/test_app.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../bin/test_app.pdb"
-  "../bin/test_app"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
